d662d3acabbe1a4d39e533800b66c1d45c876f05
s0npaRi11 <paragmahale36@gmail.com>
2024-11-06 10:03:41 +0530

    UPDATED : config files

---
+++ rollup.config.js
@@ -0,0 +1,24 @@
+import babel from "@rollup/plugin-babel";
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+
+export default {
+  input: "src/useReactFormHandler.js",
+  output: [
+    {
+      file: "dist/index.js",
+      format: "cjs",
+      exports: "named"
+    },
+    {
+      file: "dist/index.es.js",
+      format: "es"
+    }
+  ],
+  plugins: [
+    resolve(),
+    commonjs(),
+    babel({ babelHelpers: "bundled" })
+  ],
+  external: ["react"] // Don't bundle React
+};
